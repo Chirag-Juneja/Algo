ab9300e48fc97433d8408de4a17f59513d641a26
Chirag Juneja <chiragjuneja6@gmail.com>
2020-04-25 20:06:20 +0530

    Insertion Sort Reformat

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -6,24 +6,30 @@ Time Complexity: O(n)=n*n
 #include <iostream>
 #define LOG(x) std::cout << x << std::endl
 
-void insertionSort(int *array,int size){
-    for(int j=1;j<size;j++){
+void insertionSort(int *array, int size)
+{
+    for (int j = 1; j < size; j++)
+    {
         int key = array[j];
-        int i = j-1;
-        while(i >= 0 && array[i] > key){
-            array[i+1]=array[i];
+        int i = j - 1;
+
+        while (i >= 0 && array[i] > key)
+        {
+            array[i + 1] = array[i];
             i--;
         }
-        array[i+1]=key;
+        array[i + 1] = key;
     }
 }
 
-int main(int argc,char *argv[]){
-    int array[]={9,7,4,3,5,68,1,6};
-    
-    insertionSort(array,sizeof(array)/4);
+int main(int argc, char *argv[])
+{
+    int array[] = {9, 7, 4, 3, 5, 68, 1, 6};
 
-    for(int i =0;i<sizeof(array)/4;i++){
+    insertionSort(array, sizeof(array) / 4);
+
+    for (int i = 0; i < sizeof(array) / 4; i++)
+    {
         LOG(array[i]);
     }
     return 0;
